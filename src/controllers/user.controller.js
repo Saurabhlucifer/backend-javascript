2a46fb64e8e337a80d957dac18a6e4515265d275
SaurabhLucifer <saurabhsinghh2r@gmail.com>
2024-04-13 21:55:03 +0530

    added subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -192,7 +192,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
          if(!user)
          throw new ApiError(401,"Invalid refresh token")
  
- if(incomingRefreshToken !== user?.refreshToken ){
+ if(!incomingRefreshToken !== user?.refreshToken ){
      throw new ApiError(401,"Refresh token expired")
  }
     const options = {
